b90c1e056b181a0bf053536f97bc4ea9bb7b8098
Spence Nelson <spenjmn@me.com>
2016-11-01 08:35:55 -0400

    mobile styles

---
+++ js/main.js
@@ -0,0 +1,238 @@
+$( document ).ready(function() {
+            $( "a.yunniy" ).hover(
+                function() {
+                $( "img.yunniy" ).addClass( "hover" );
+                }, function() {
+                $( "img.yunniy" ).removeClass( "hover" );
+                }
+            );
+            $( "a.yumingl" ).hover(
+                function() {
+                $( "img.yumingl" ).addClass( "hover" );
+                }, function() {
+                $( "img.yumingl" ).removeClass( "hover" );
+                }
+            );
+            $( "a.theag" ).hover(
+                function() {
+                $( "img.theag" ).addClass( "hover" );
+                }, function() {
+                $( "img.theag" ).removeClass( "hover" );
+                }
+            );
+            $( "a.stephenk" ).hover(
+                function() {
+                $( "img.stephenk" ).addClass( "hover" );
+                }, function() {
+                $( "img.stephenk" ).removeClass( "hover" );
+                }
+            );
+            $( "a.spencen" ).hover(
+                function() {
+                $( "img.spencen" ).addClass( "hover" );
+                }, function() {
+                $( "img.spencen" ).removeClass( "hover" );
+                }
+            );
+            $( "a.sophiar" ).hover(
+                function() {
+                $( "img.sophiar" ).addClass( "hover" );
+                }, function() {
+                $( "img.sophiar" ).removeClass( "hover" );
+                }
+            );
+            $( "a.shirleyz" ).hover(
+                function() {
+                $( "img.shirleyz" ).addClass( "hover" );
+                }, function() {
+                $( "img.shirleyz" ).removeClass( "hover" );
+                }
+            );
+            $( "a.sarahh" ).hover(
+                function() {
+                $( "img.sarahh" ).addClass( "hover" );
+                }, function() {
+                $( "img.sarahh" ).removeClass( "hover" );
+                }
+            );
+            $( "a.ruid" ).hover(
+                function() {
+                $( "img.ruid" ).addClass( "hover" );
+                }, function() {
+                $( "img.ruid" ).removeClass( "hover" );
+                }
+            );
+            $( "a.paigeb" ).hover(
+                function() {
+                $( "img.paigeb" ).addClass( "hover" );
+                }, function() {
+                $( "img.paigeb" ).removeClass( "hover" );
+                }
+            );
+            $( "a.noram" ).hover(
+                function() {
+                $( "img.noram" ).addClass( "hover" );
+                }, function() {
+                $( "img.noram" ).removeClass( "hover" );
+                }
+            );
+            $( "a.nicolef" ).hover(
+                function() {
+                $( "img.nicolef" ).addClass( "hover" );
+                }, function() {
+                $( "img.nicolef" ).removeClass( "hover" );
+                }
+            );
+            $( "a.mjd" ).hover(
+                function() {
+                $( "img.mjd" ).addClass( "hover" );
+                }, function() {
+                $( "img.mjd" ).removeClass( "hover" );
+                }
+            );
+            $( "a.mackenzies" ).hover(
+                function() {
+                $( "img.mackenzies" ).addClass( "hover" );
+                }, function() {
+                $( "img.mackenzies" ).removeClass( "hover" );
+                }
+            );
+            $( "a.maggieg" ).hover(
+                function() {
+                $( "img.maggieg" ).addClass( "hover" );
+                }, function() {
+                $( "img.maggieg" ).removeClass( "hover" );
+                }
+            );
+            $( "a.londonz" ).hover(
+                function() {
+                $( "img.londonz" ).addClass( "hover" );
+                }, function() {
+                $( "img.londonz" ).removeClass( "hover" );
+                }
+            );
+            $( "a.lameesam" ).hover(
+                function() {
+                $( "img.lameesam" ).addClass( "hover" );
+                }, function() {
+                $( "img.lameesam" ).removeClass( "hover" );
+                }
+            );
+            $( "a.karle" ).hover(
+                function() {
+                $( "img.karle" ).addClass( "hover" );
+                }, function() {
+                $( "img.karle" ).removeClass( "hover" );
+                }
+            );
+            $( "a.kariymam" ).hover(
+                function() {
+                $( "img.kariymam" ).addClass( "hover" );
+                }, function() {
+                $( "img.kariymam" ).removeClass( "hover" );
+                }
+            );
+            $( "a.juk" ).hover(
+                function() {
+                $( "img.juk" ).addClass( "hover" );
+                }, function() {
+                $( "img.juk" ).removeClass( "hover" );
+                }
+            );
+            $( "a.jennyj" ).hover(
+                function() {
+                $( "img.jennyj" ).addClass( "hover" );
+                }, function() {
+                $( "img.jennyj" ).removeClass( "hover" );
+                }
+            );
+            $( "a.jenea" ).hover(
+                function() {
+                $( "img.jenea" ).addClass( "hover" );
+                }, function() {
+                $( "img.jenea" ).removeClass( "hover" );
+                }
+            );
+            $( "a.hongjingz" ).hover(
+                function() {
+                $( "img.hongjingz" ).addClass( "hover" );
+                }, function() {
+                $( "img.hongjingz" ).removeClass( "hover" );
+                }
+            );
+            $( "a.ericar" ).hover(
+                function() {
+                $( "img.ericar" ).addClass( "hover" );
+                }, function() {
+                $( "img.ericar" ).removeClass( "hover" );
+                }
+            );
+            $( "a.davidk" ).hover(
+                function() {
+                $( "img.davidk" ).addClass( "hover" );
+                }, function() {
+                $( "img.davidk" ).removeClass( "hover" );
+                }
+            );
+            $( "a.daniels" ).hover(
+                function() {
+                $( "img.daniels" ).addClass( "hover" );
+                }, function() {
+                $( "img.daniels" ).removeClass( "hover" );
+                }
+            );
+            $( "a.connord" ).hover(
+                function() {
+                $( "img.connord" ).addClass( "hover" );
+                }, function() {
+                $( "img.connord" ).removeClass( "hover" );
+                }
+            );
+            $( "a.clairex" ).hover(
+                function() {
+                $( "img.clairex" ).addClass( "hover" );
+                }, function() {
+                $( "img.clairex" ).removeClass( "hover" );
+                }
+            );
+            $( "a.christinah" ).hover(
+                function() {
+                $( "img.christinah" ).addClass( "hover" );
+                }, function() {
+                $( "img.christinah" ).removeClass( "hover" );
+                }
+            );
+            $( "a.christians" ).hover(
+                function() {
+                $( "img.christians" ).addClass( "hover" );
+                }, function() {
+                $( "img.christians" ).removeClass( "hover" );
+                }
+            );
+            $( "a.andrewv" ).hover(
+                function() {
+                $( "img.andrewv" ).addClass( "hover" );
+                }, function() {
+                $( "img.andrewv" ).removeClass( "hover" );
+                }
+            );
+            $( "a.alyser" ).hover(
+                function() {
+                $( "img.alyser" ).addClass( "hover" );
+                }, function() {
+                $( "img.alyser" ).removeClass( "hover" );
+                }
+            );
+            $( "a.alexav" ).hover(
+                function() {
+                $( "img.alexav" ).addClass( "hover" );
+                }, function() {
+                $( "img.alexav" ).removeClass( "hover" );
+                }
+            );
+
+
+
+
+
+        });
